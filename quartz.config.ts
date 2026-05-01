d764e153eb71955655e09b4c25f4a9ff5711bc32
EneneAea <neri-enea@hotmail.it>
2026-05-01 14:41:04 +0200

    Quartz sync: May 1, 2026, 2:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -43,7 +43,7 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#418939",
+          secondary: "#50a347",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
